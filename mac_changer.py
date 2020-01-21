c4a93e552643b9c9693468e7c2c035a78f753625
hanimohammedhmk <56012201+hanimohammedhmk@users.noreply.github.com>
2020-01-21 03:11:46 -0500

    Add files via upload

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 def print_motd():
     print("""
@@ -16,6 +17,9 @@ def get_arguments():
     parser.add_option("-i", "--interface", dest="interface", help="interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac_address", help="MAC address")
     (options, arguments) = parser.parse_args()
+
+    #print(options.new_mac_address)
+    #print(options.interface)
     if not options.interface:
         print_motd()
         parser.error("\n[-]Please specify an interface to change MAC address.\n use --help / -h for more information")
@@ -24,13 +28,32 @@ def get_arguments():
         parser.error("\n[-]Please specify a MAC address to change MAC address.\n use --help / -h for more information")
     return options
 
-
 def change_mac(interface, new_mac_address):
     print_motd()
-    print("[+] changing MAC address of " + interface + " to " + new_mac_address)
+    print("[+]Changing MAC address of " + interface + " to " + new_mac_address)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac_address])
     subprocess.call(["ifconfig", interface, "up"])
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_search_result.group(0):
+        return mac_search_result.group(0)
+    else:
+        print("[-]Could not find MAC address")
+
+
 options = get_arguments()
+
+current_mac = get_current_mac(options.interface)
+print("Current MAC address : "+ str(current_mac))
+
 change_mac(options.interface, options.new_mac_address)
+
+current_mac = get_current_mac(options.interface)
+
+if current_mac == options.new_mac_address:
+    print("[+]Successfully changed MAC address to " + current_mac )
+else :
+    print("[-]Sorry failed to change the MAC address")
